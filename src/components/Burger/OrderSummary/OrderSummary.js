77162d1b2e6058f9c1e4194ef548796f2ed74ed6
Efecan YILMAZ <efecanyilmaz@outlook.com>
2020-05-11 20:50:19 +0200

    final

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux2';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients).map(igKey => { return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}:</span> {props.ingredients[igKey]}</li> });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A super amazing burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)} €</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType='Danger' clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType='Success' clicked={props.purchaseContinue}>CONTINUE</Button>
-        </Aux>
-    );
+class OrderSummary extends Component {
+    // should be a funct. component. class comp. here for debug reasons.
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => { return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}:</span> {this.props.ingredients[igKey]}</li> });
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A super amazing burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)} €</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType='Danger' clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType='Success' clicked={this.props.purchaseContinue}>CONTINUE</Button>
+            </Aux>
+        );
+    } 
 }
 
-export default orderSummary;
+export default OrderSummary;
