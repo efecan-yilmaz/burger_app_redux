2aaa70dead1b266415e857cffe0f4b6f60a30d9f
Efe <efecanyilmaz@outlook.com>
2020-07-13 20:40:54 +0200

    routing finished

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import {Route, Switch} from 'react-router-dom';
+import Orders from './containers/Orders/Orders';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Layout>
         <Switch> // Not necessary since we use exact on BurgerBuilder. if we didn't swtich must be used and order is important
           <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
           <Route path="/" exact component={BurgerBuilder} />
         </Switch>
       </Layout>
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,8 +4,8 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem link="/" active>Burger Builder</NavigationItem>
-        <NavigationItem link="/" caption="">Checkout</NavigationItem>
+        <NavigationItem link="/" exact active>Burger Builder</NavigationItem>
+        <NavigationItem link="/orders" caption="Orders">Orders</NavigationItem>
     </ul>
 );
 
